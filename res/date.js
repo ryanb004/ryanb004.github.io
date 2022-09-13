bb77882f6e7af6bc20aa8ccd0d3d01164a27ef2f
ryanb004 <112939771+ryanb004@users.noreply.github.com>
2022-09-13 19:08:45 +1000

    a

--- res/date.js
+++ res/date.js
@@ -14,7 +14,7 @@ const nth = function(d) {
 
 
 
-let english1 = new Date('10/12/2022');
+let english1 = new Date('2022-10-12T09:50');
 let english2 = new Date('10/13/2022');
 let ext2 = new Date('10/20/2022');
 let ext1 = new Date('10/24/2022');
@@ -25,15 +25,15 @@ let date = new Date();
 var current_date = date.getDate()+"-"+(date.getMonth()+1)+"-"+date.getFullYear();
 document.getElementById("p1").innerHTML = current_date +" " +english1;
 if (english1 > date) {test = english1; 
-document.getElementById("p1").innerHTML = "English Paper 1"+" "+ (weekday[test.getDay()] + " " + test.getDate()+nth(test.getDate) + " " + test.toLocaleString('default', { month: 'long' }))}
+document.getElementById("p1").innerHTML = "English Paper 1"+" "+ (weekday[test.getDay()] + " " + test.getDate()+nth(test.getDate) + " " + test.toLocaleString('default', { month: 'long' })+" at 9:50AM")}
 else if (english2 > date) {test = english2; 
-document.getElementById("p1").innerHTML = "English Paper 2"+" "+ (weekday[test.getDay()] + " " + test.getDate()+nth(test.getDate) + " " + test.toLocaleString('default', { month: 'long' }))}
+document.getElementById("p1").innerHTML = "English Paper 2"+" "+ (weekday[test.getDay()] + " " + test.getDate()+nth(test.getDate) + " " + test.toLocaleString('default', { month: 'long' }))+" at 9:25AM"}
 else if (ext2 > date) {test = ext2; 
-document.getElementById("p1").innerHTML = "Maths Extension 2"+" "+ (weekday[test.getDay()] + " " + test.getDate()+nth(test.getDate) + " " + test.toLocaleString('default', { month: 'long' }))}
+document.getElementById("p1").innerHTML = "Maths Extension 2"+" "+ (weekday[test.getDay()] + " " + test.getDate()+nth(test.getDate) + " " + test.toLocaleString('default', { month: 'long' })+ " at 1:50PM")}
 else if (ext1 > date) {test = ext1;
-document.getElementById("p1").innerHTML = "Maths Extension 1"+" "+ (weekday[test.getDay()] + " " + test.getDate()+nth(test.getDate) + " " + test.toLocaleString('default', { month: 'long' }))}
+document.getElementById("p1").innerHTML = "Maths Extension 1"+" "+ (weekday[test.getDay()] + " " + test.getDate()+nth(test.getDate) + " " + test.toLocaleString('default', { month: 'long' })+" at 9:30AM")}
 else if (multimedia > date) {test = multimedia; 
-document.getElementById("p1").innerHTML = "Multimedia"+" "+ (weekday[test.getDay()] + " " + test.getDate()+nth(test.getDate) + " " + test.toLocaleString('default', { month: 'long' }))}
+document.getElementById("p1").innerHTML = "Multimedia"+" "+ (weekday[test.getDay()] + " " + test.getDate()+nth(test.getDate) + " " + test.toLocaleString('default', { month: 'long' })+" at 9:25AM")}
 else if (chemistry > date) {test = chemistry; 
-document.getElementById("p1").innerHTML = "Chemistry"+" "+ (weekday[test.getDay()] + " " + test.getDate()+nth(test.getDate) + " " + test.toLocaleString('default', { month: 'long' }))}
+document.getElementById("p1").innerHTML = "Chemistry"+" "+ (weekday[test.getDay()] + " " + test.getDate()+nth(test.getDate) + " " + test.toLocaleString('default', { month: 'long' })+" at 9:25AM")}
 else {document.getElementByID("p1").innerHTML = "All done!"}
